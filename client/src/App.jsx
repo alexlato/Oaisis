3654c88f784e0f9d36ed683b161119c6f7cb6496
alexlato <86857302+alexlato@users.noreply.github.com>
2023-05-02 22:46:49 -0700

    Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import { Home, CreatePost } from "./pages";
 const App = () => {
   return (
     <BrowserRouter>
-      <header className="w-full flex items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to="/">
           <img
             src={logo}
@@ -14,10 +14,24 @@ const App = () => {
             className="w-14 object-contain rounded-md"
           />
         </Link>
-        <div class="flex justify-between items-center sm:px-8 px-4 py-4 font-semibold text-xl">
+
+        <div class="w-full sm:px-8 px-4 py-4 font-semibold text-xl text-green-600 text-2xl">
           <p>Oaisis.io</p>
         </div>
+
+        <Link
+          to="/create-post"
+          className="items-center flex justify-between font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+        >
+          Create
+        </Link>
       </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 };
